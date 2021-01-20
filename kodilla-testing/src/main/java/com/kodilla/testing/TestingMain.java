9d57652673306b7a95da52b5b277be8d619331d1
Sylwia Krzymowska <s.krzymowska@gmail.com>
2021-01-20 22:15:10 +0100

    Module 6.2 - fixed

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,6 +5,27 @@ import com.kodilla.calculator.MyCalculator;
 public class TestingMain {
 
     public static void main(String[] args) {
+        int a = 18;
+        int b = 12;
 
+        MyCalculator myCalculator = new MyCalculator(a, b);
+
+        int resultAdd;
+        int resultSubstract;
+
+        resultAdd = myCalculator.add();
+        resultSubstract = myCalculator.substract();
+
+        if (resultAdd == a + b) {
+            System.out.println("Adding successful, " + a + " + " + b + " is: " + resultAdd);
+        } else {
+            System.out.println("Error with calculator - adding");
+        }
+
+        if (resultSubstract == a - b) {
+            System.out.println("Substracting successful, " + a + " - " + b + " is: " + resultSubstract);
+        } else {
+            System.out.println("Error with calculator - substracting");
+        }
     }
 }
