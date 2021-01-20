68216fbe02d1be4514d1a7c2f3483acf6439b1a2
Sylwia Krzymowska <s.krzymowska@gmail.com>
2021-01-20 17:37:59 +0100

    Pierwszy test jednostkowy

---
+++ kodilla-testing/src/main/java/com/kodilla/calculator/MyCalculator.java
@@ -0,0 +1,20 @@
+package com.kodilla.calculator;
+
+public class MyCalculator {
+    private int a;
+    private int b;
+
+    public MyCalculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add() {
+        int c = a + b;
+        return c;
+    }
+    public int substract() {
+        int c = a - b;
+        return c;
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.calculator.MyCalculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        int a = 18;
+        int b = 12;
+
+        MyCalculator myCalculator = new MyCalculator(a, b);
+
+        int resultAdd;
+        int resultSubstract;
+
+        resultAdd = myCalculator.add();
+        resultSubstract = myCalculator.substract();
+
+        if (resultAdd == a + b) {
+            System.out.println("Adding successful, " + a + " + " + b + " is: " + resultAdd);
+        } else {
+            System.out.println("Error with calculator - adding");
+        }
+
+        if (resultSubstract == a - b) {
+            System.out.println("Substracting successful, " + a + " - " + b + " is: " + resultSubstract);
+        } else {
+            System.out.println("Error with calculator - substracting");
+        }
     }
 }
